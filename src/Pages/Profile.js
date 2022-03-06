8af77b71423d8f9a4b25b148a39f100897a7c15b
Daniel Adea <daniel.adea@risksciences.ucla.edu>
2022-03-06 15:26:29 -0800

    filled out profile data
    pulled from api and formatted info into a table

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -9,7 +9,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { makeStyles } from '@mui/styles';
+import axios from 'axios';
 import logo from '../assets/Leetracer_plain.png'
+
+const API_URL = process.env.REACT_APP_API_URL
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -31,21 +34,48 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function labelData(name, difficulty, type, time) {
-  return { name, difficulty, type, time };
+function labelData(title, difficulty, time, winner) {
+  return { title, difficulty, time, winner };
 }
 
 //hopefully this can be easily changed to format the backend output?
+//fake data
 const rows = [
-  labelData('Compare Version Numbers', 'Medium', 'Array', '14:54'),
-  labelData('Two Sum', 'Easy', 'Array', '11:20'),
+  labelData('Compare Version Numbers', 'Medium', '14:54', 'User1'),
+  labelData('Two Sum', 'Easy', '11:20', "User3"),
 ];
 
-const user = "Naruto";
-const pass = "Ramen";
+
 
 function Profile(props) {
 	const classes = useStyles();
+	const [data, setData] = React.useState([]);
+	React.useEffect(async ()=> {        
+        try {
+           	let username = localStorage.getItem('username')
+           	let res = await axios.get(`${API_URL}/stats/user/${username}`)
+            setData(res.data.races);            
+        } catch(e){
+           	console.log('error wtf')
+        }
+	}, []);
+
+    const DisplayData=data.map(
+        (row)=>{
+            return(
+                <TableBody>
+		            <StyledTableRow key={row.title}>
+		              <StyledTableCell align= "center" component="th" scope="row">
+		                {row.title}
+		              </StyledTableCell>
+		              <StyledTableCell align="center">{row.difficulty}</StyledTableCell>
+		              <StyledTableCell align="center">{row.timeToSolve}</StyledTableCell>
+		              <StyledTableCell align="center">{row.winner}</StyledTableCell>
+		            </StyledTableRow> 
+                </TableBody>
+            )
+        }
+    )
     return (
         <div style={{margin: '50px 40px 0px 40px'}}>
             <div className = {classes.container}>
@@ -55,30 +85,17 @@ function Profile(props) {
 		      <Table sx={{ minWidth: 700 }} aria-label="customized table">
 		        <TableHead>
 		          <TableRow>
-		            <StyledTableCell align="center">Name</StyledTableCell>
+		            <StyledTableCell align="center">Title</StyledTableCell>
 		            <StyledTableCell align="center">Difficulty</StyledTableCell>
-		            <StyledTableCell align="center">Problem Type</StyledTableCell>
 		            <StyledTableCell align="center">Time</StyledTableCell>
+		            <StyledTableCell align="center">Winner</StyledTableCell>
 		          </TableRow>
 		        </TableHead>
-		        <TableBody>
-		          {rows.map((row) => (
-		            <StyledTableRow key={row.name}>
-		              <StyledTableCell align= "center" component="th" scope="row">
-		                {row.name}
-		              </StyledTableCell>
-		              <StyledTableCell align="center">{row.difficulty}</StyledTableCell>
-		              <StyledTableCell align="center">{row.type}</StyledTableCell>
-		              <StyledTableCell align="center">{row.time}</StyledTableCell>
-		            </StyledTableRow>
-		          ))}
-		        </TableBody>
+		
+		        	{console.log('hiyo')}
+		          	{DisplayData}
 		      </Table>
 		    </TableContainer>
-		    </div>
-		    <div className={classes.container} >
-		    	<p>Username: {user} </p>
-		    	<p>Password: {pass} </p>
 		    </div>
         </div>
     )
